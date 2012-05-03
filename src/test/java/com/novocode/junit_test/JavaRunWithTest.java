a45993329eab7766b2a08cb0a6ba193c74c7766f
Stefan Zeiger <szeiger@novocode.com>
2012-05-03 10:58:22 +0200

    Add Java tests

---
+++ src/test/java/com/novocode/junit_test/JavaRunWithTest.java
@@ -0,0 +1,10 @@
+package com.novocode.junit_test;
+
+import org.junit.runner.RunWith;
+
+@RunWith(MyRunner.class)
+public class JavaRunWithTest {
+  public void myTest() {
+    System.out.println("In JavaRunWithTest.myTest");
+  }
+}
---
+++ src/test/java/com/novocode/junit_test/JavaTest.java
@@ -0,0 +1,9 @@
+package com.novocode.junit_test;
+
+import org.junit.Test;
+
+public class JavaTest {
+  @Test public void myJavaTest() {
+    System.out.println("In JavaTest.myJavaTest");
+  }
+}
